2a7c26d7adc3fb0ced084625b6ad93db8b67e7b9
kalugy <danieltrujillo901@gmail.com>
2018-05-03 20:27:34 -0500

    mas diseño 2

--- resources/views/introduccion.blade.php
+++ resources/views/introduccion.blade.php
@@ -11,12 +11,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
-<input id="toggle-one" checked type="checkbox">
-<script>
-  $(function() {
-    $('#toggle-one').bootstrapToggle();
-  })
-</script>
+
 
 <p><label for="mensaje">
 
